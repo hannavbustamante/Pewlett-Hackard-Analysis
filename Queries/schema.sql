02dce704dd56c81c6b745e5e4683cc14942fd1af
hannavbustamante <hannavbustamante@gmail.com>
2021-10-11 11:49:29 -0400

    Adding queries for retirement_info.csv

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -45,8 +45,8 @@ CREATE TABLE Titles (
 );
 
 CREATE TABLE Dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
