5decc57a36f1e95004258da2be3de1b8a1d25a32
hannavbustamante <hannavbustamante@gmail.com>
2021-10-12 23:03:22 -0400

    Adding Deliverable 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,55 @@
+---------------------------------------------------------------------------------------------------------------------
+--Deliverable 1: 
+---------------------------------------------------------------------------------------------------------------------
+
+-- The Number of Retiring Employees by Title.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS ti
+	 ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- The Number of Retiring Employees by Title (No Duplicates).
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- The number of employees by their most recent job title who are about to retire.
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC
+
+-----------------------------------------------------------------------------------------------------------------------------
+--Deliverable 2: 
+-----------------------------------------------------------------------------------------------------------------------------
+
+-- The Employees Eligible for the Mentorship Program. (Current employees who were born between January 1, 1965 and December 31, 1965.)
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+		ON (de.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
