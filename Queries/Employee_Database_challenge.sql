2d079fc4ab696e4435f8648adaa066ddd4614691
hannavbustamante <hannavbustamante@gmail.com>
2021-10-13 13:53:02 -0400

    Adding MentorshipEligibility Figure

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -44,7 +44,7 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	de.from_date,
 	de.to_date,
 	ti.title
-INTO mentorship_eligibilty
+INTO mentorship_eligibility
 FROM employees AS e
 	INNER JOIN dept_emp AS de
 		ON (e.emp_no = de.emp_no)
